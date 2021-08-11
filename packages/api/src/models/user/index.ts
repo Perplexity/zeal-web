baee026c124968cf1928293d4107c817618e7dbf
Ryan Pearce <perplexity@outlook.com>
2021-08-11 21:21:53 +0100

    chore: linting

--- packages/api/src/models/user/index.ts
+++ packages/api/src/models/user/index.ts
@@ -11,7 +11,7 @@ export const create = async (user: User): Promise<number> => {
 		throw new Error("Email address is already in use");
 	}
 	const query =
-    "INSERT INTO users (username, password, email_address) VALUES (?, ?, ?)";
+		"INSERT INTO users (username, password, email_address) VALUES (?, ?, ?)";
 	const [result] = await db
 		.promise()
 		.query(query, [user.username, user.password, user.email_address]);
@@ -81,7 +81,7 @@ export const emailExists = async (email: string): Promise<boolean> => {
 export const logLogin = async (userId: number, ipAddress: string): Promise<number> => {
 	console.log(`User ${userId} logged in with IP: ${ipAddress}`);
 	const query =
-    "INSERT INTO login_logs (user_id, timestamp, ip_address) VALUES (?, ?, ?)";
+		"INSERT INTO login_logs (user_id, timestamp, ip_address) VALUES (?, ?, ?)";
 	const [result] = await db
 		.promise()
 		.query(query, [userId, moment.now().valueOf(), ipAddress]);
