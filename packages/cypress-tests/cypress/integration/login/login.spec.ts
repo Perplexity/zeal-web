25716f8aa8391979612f8ef34be6fbfda40edd3a
Ryan Pearce <perplexity@outlook.com>
2021-08-13 21:41:42 +0100

    feat: linting and proper eslint config

--- packages/cypress-tests/cypress/integration/login/login.spec.ts
+++ packages/cypress-tests/cypress/integration/login/login.spec.ts
@@ -1,13 +1,13 @@
-context('Zeal login page', () => {
-  beforeEach(() => {
-    cy.setCookie('x-cypress-test', "true");
-    cy.visit('/');
-  });
+context("Zeal login page", () => {
+	beforeEach(() => {
+		cy.setCookie("x-cypress-test", "true");
+		cy.visit("/");
+	});
 
-  describe("When opening the login page", () => {
-    it("Should render the login form", () => {
-      cy.get('#username').should('exist');
-      cy.get('#password').should('exist');
-    })
-  });
-})
+	describe("When opening the login page", () => {
+		it("Should render the login form", () => {
+			cy.get("#username").should("exist");
+			cy.get("#password").should("exist");
+		});
+	});
+});
