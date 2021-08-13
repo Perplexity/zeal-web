659e95d86f2d8d5e7ca660c2257b353450438f81
Ryan Pearce <perplexity@outlook.com>
2021-08-13 21:33:15 +0100

    feat: basic login test

---
+++ packages/cypress-tests/cypress/integration/login/login.spec.ts
@@ -0,0 +1,13 @@
+context('Zeal login page', () => {
+  beforeEach(() => {
+    cy.setCookie('x-cypress-test', "true");
+    cy.visit('/');
+  });
+
+  describe("When opening the login page", () => {
+    it("Should render the login form", () => {
+      cy.get('#username').should('exist');
+      cy.get('#password').should('exist');
+    })
+  });
+})
