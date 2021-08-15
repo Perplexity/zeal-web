42013c9711640d80618ae00420cb7f3b3e780135
Ryan Pearce <perplexity@outlook.com>
2021-08-15 12:57:31 +0100

    feat: added dashboard tests

---
+++ packages/cypress-tests/cypress/integration/dashboard/dashboard.spec.ts
@@ -0,0 +1,39 @@
+import { authenticateUser } from "../../support/api/authenticate-user";
+import * as validUser from "../../fixtures/valid-user.json";
+import * as userWithLicense from "../../fixtures/user-with-license.json";
+
+context("Zeal dashboard", () => {
+	beforeEach(() => {
+		authenticateUser(validUser.username, validUser.password);
+		cy.visit("/dashboard/home");
+	});
+
+	context("User with valid license", () => {
+		beforeEach(() => {
+			cy.intercept("GET", "**/user/me", { body: userWithLicense });
+		});
+		describe("When the dashboard has been loaded", () => {
+			it("Should display their license information", () => {
+				const licenseCard = () => cy.get("#license-card");
+				licenseCard().should("contain.text", `Tier: ${userWithLicense.license.name}`);
+				licenseCard().should("contain.text", `Job Limit: ${userWithLicense.license.job_limit}`);
+				licenseCard().should("contain.text", `Thread Limit: ${userWithLicense.license.thread_limit}`);
+				licenseCard().should("contain.text", `Combo Limit: ${Number(userWithLicense.license.combo_limit).toLocaleString()}`);
+			});
+		});
+	});
+
+	context("User without license", () => {
+		beforeEach(() => {
+			cy.intercept("GET", "**/user/me", { fixture: "user-without-license.json" });
+		});
+		describe("When the dashboard has been loaded", () => {
+			it("Should prompt the user to purchase/redeem a license", () => {
+				const licenseCard = () => cy.get("#license-card");
+				licenseCard().should("contain.text", "You do not currently have an active license.");
+				cy.get("#purchase-license").should("be.visible");
+				cy.get("#redeem-license").should("be.visible");
+			});
+		});
+	});
+});
